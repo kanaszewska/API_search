aa0defcfbfce3adc7c5c406fb172db4c175bda68
Dominika <kanaszewskadominika@gmail.com>
2022-09-28 16:24:07 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+import logo from './styles/images/logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className='header'>
+      <img src={logo} alt='/'/>
       <h1>Turist</h1>
       </div>
       <div className='section'>
